06f936dc8ea4f4fa7935333e7bce494fad698330
xuyi <yi.xu@ymm56.com>
2018-09-18 20:16:37 +0800

    commit

--- mall-common/src/main/java/com/jy/response/Result.java
+++ mall-common/src/main/java/com/jy/response/Result.java
@@ -5,10 +5,15 @@ import com.jy.constant.ResponseEnum;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * 返回基类
+ * @param <T>
+ */
 public class Result<T> {
     private int code;
     private String msg;
-    private List<T> obj;
+    private List<T> objs;//返回集合
+    private T obj;//返回对象，区分集合避免繁琐操作
 
     public void ok(){
         code = ResponseEnum.Response_Status_Ok.getCode();
@@ -31,11 +36,19 @@ public class Result<T> {
         this.msg = msg;
     }
 
-    public List<T> getObj() {
+    public List<T> getObjs() {
+        return objs;
+    }
+
+    public void setObjs(List<T> objs) {
+        this.objs = objs;
+    }
+
+    public T getObj() {
         return obj;
     }
 
-    public void setObj(List<T> obj) {
+    public void setObj(T obj) {
         this.obj = obj;
     }
 }
