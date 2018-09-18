ee0f58c5fd78df8c8b71d1aa2293287f68fab6fe
xuyi <yi.xu@ymm56.com>
2018-09-18 20:17:46 +0800

    commit

--- mall-admin/mall-admin-web/src/main/java/com/jy/controller/page/PageController.java
+++ mall-admin/mall-admin-web/src/main/java/com/jy/controller/page/PageController.java
@@ -1,19 +1,16 @@
 package com.jy.controller.page;
 
-import com.jy.aop.config.annoation.Config;
-import com.jy.aop.config.annoation.Login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * 视图跳转类
+ */
 @Controller
-@Login("admin")
 public class PageController {
-
-    @Config(path = "/xuyi/server")
-    private String c;
 
     @RequestMapping(value = "/page/{url}",method = RequestMethod.GET)
     public String toPage(@PathVariable(value = "url")String url){
--- mall-admin/mall-admin-web/src/main/java/com/jy/controller/pic/PictureController.java
+++ mall-admin/mall-admin-web/src/main/java/com/jy/controller/pic/PictureController.java
@@ -1,15 +1,22 @@
 package com.jy.controller.pic;
 
 import com.jy.response.Result;
+import com.jy.service.img.UploadImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class PictureController {
+
+    @Autowired
+    UploadImageService uploadImageService;
+
     @RequestMapping(value = "/pic/upload",method = RequestMethod.POST)
-    public Result<String> picUpload(MultipartFile multipartFile){
-        return null;
+    public Result<String> picUpload(@RequestParam("file") MultipartFile file){
+        return uploadImageService.uploadImage(file);
     }
 }
